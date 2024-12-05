2af614fa6f1880de4e25a80096b4de547f038e7d
LICF01 <lucascubilla@gmail.com>
2024-12-05 07:54:50 -0300

    Display downloaded image

--- app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -28,11 +28,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.marsphotos.R
+import com.example.marsphotos.model.MarsPhoto
 import com.example.marsphotos.ui.theme.MarsPhotosTheme
 
 @Composable
@@ -43,10 +48,8 @@ fun HomeScreen(
 ) {
     when (marsUiState) {
         is MarsUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
-        is MarsUiState.Success -> ResultScreen(
-            marsUiState.photos, modifier = modifier.fillMaxWidth()
-        )
-        is MarsUiState.Error -> ErrorScreen( modifier = modifier.fillMaxSize())
+        is MarsUiState.Success -> MarsPhotoCard(photo = marsUiState.photos, modifier = modifier.fillMaxSize())
+        else  -> ErrorScreen(modifier = modifier.fillMaxSize())
     }
 }
 
@@ -92,6 +95,20 @@ fun ResultScreen(photos: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun MarsPhotoCard(photo: MarsPhoto, modifier: Modifier = Modifier) {
+    AsyncImage(
+        model = ImageRequest.Builder(context = LocalContext.current).data(photo.imgSrc)
+            .crossfade(true).build(),
+        contentDescription = stringResource(R.string.mars_photo),
+        error = painterResource(R.drawable.ic_broken_image),
+        placeholder = painterResource(R.drawable.loading_img),
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun LoadingScreenPreview() {
@@ -115,3 +132,4 @@ fun PhotosGridScreenPreview() {
         ResultScreen(stringResource(R.string.placeholder_success))
     }
 }
+
